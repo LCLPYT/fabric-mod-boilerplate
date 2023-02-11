d2a427b177ec4f4ec265f6f6bdf190fdd61d6863
LCLP <lukas@lclpnet.work>
2023-02-11 17:28:20 +0100

    use modid for mod logger

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -8,8 +8,8 @@ import org.slf4j.LoggerFactory;
 
 public class ExampleMod implements ModInitializer {
 
-	public static final Logger LOGGER = LoggerFactory.getLogger("testmod");
 	public static final String MOD_ID = "testmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
